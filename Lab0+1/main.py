a5114f699c82406340cb58038cf098aebd3b4db6
brandonkim5199 <brandonkim5199@gmail.com>
2026-09-22 21:21:29 -0400

    lab 1 part 1

--- Lab0+1/main.py
+++ Lab0+1/main.py
@@ -12,12 +12,48 @@ builtin_led.value(1) # Built in LED - 1 is on
 np_led[0] = (0, 0, 0) # NeoPixel LED - (0,0,0) is off
 np_led.write()
 
-while True:
-	builtin_led.value(not builtin_led.value())
-	if np_led[0] == (0, 0 ,0):
-		np_led[0] = (255, 255, 255)
-	else:
-		np_led[0] = (0, 0, 0)
+# while True:
+# 	builtin_led.value(not builtin_led.value())
+# 	if np_led[0] == (0, 0 ,0):
+# 		np_led[0] = (255, 255, 255)
+# 	else:
+# 		np_led[0] = (0, 0, 0)
 	
-	np_led.write()
-	utime.sleep_ms(100)
+# 	np_led.write()
+# 	utime.sleep_ms(100)
+
+# lab 1 part 1
+while True:
+# S
+    for i in range(3):
+        np_led[0] = (225, 0, 0)
+        np_led.write()
+        utime.sleep(0.5)
+
+        np_led[0] = (0, 0, 0)
+        np_led.write()
+        utime.sleep(0.5)
+
+    utime.sleep(1)
+# O
+    for i in range(3):
+        np_led[0] = (225, 0, 0)
+        np_led.write()
+        utime.sleep(1.0)
+
+        np_led[0] = (0, 0, 0)
+        np_led.write()
+        utime.sleep(0.5)
+
+    utime.sleep(1)
+# S
+    for i in range(3):
+        np_led[0] = (225, 0, 0)
+        np_led.write()
+        utime.sleep(0.5)
+
+        np_led[0] = (0, 0, 0)
+        np_led.write()
+        utime.sleep(0.5)
+		
+    utime.sleep(2)
